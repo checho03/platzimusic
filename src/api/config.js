f2eb5d4ad1702602c128571609f36a74f5d7e50e
JRodolfo <joserodolfosandovalpineda@gmail.com>
2018-12-01 12:08:36 -0600

    Initial commit

---
+++ src/api/config.js
@@ -0,0 +1,12 @@
+export default {
+	appname: 'platzimusic',
+	apiKey: '39e5b3d6ae4b8a45a52fda1961dfb4ae',
+	secret: '149ae121268b1c3e4ead91ae282d2ca3',
+	resgisteredTo: 'jrsandoval3'
+}
+
+
+// Application name 	platzimusic
+// API key 	39e5b3d6ae4b8a45a52fda1961dfb4ae
+// Shared secret 	149ae121268b1c3e4ead91ae282d2ca3
+// Registered to 	jrsandoval3
